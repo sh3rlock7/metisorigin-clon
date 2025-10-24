f1eb5e08e5a2dd7bf5007a312e7bf6a9dbb0ade3
Sh3rlock7K <kevindebonis7@hotmail.com>
2025-10-24 13:56:07 +0200

    Proyect configurated, assets from metis-origin in public added

---
+++ app/ui/Navbar.tsx
@@ -0,0 +1,121 @@
+"use client";
+import { AnimatePresence } from "motion/react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { RiCloseFill, RiInstagramFill, RiLinkedinFill,  RiMenuFill } from "react-icons/ri";
+import { motion } from "motion/react"
+
+
+export const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+            setScrolled(true);
+            } else {
+            setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
+    return (
+    //Mobile Navbar
+    <>
+        <div className="flex relative w-full justify-between md:hidden items-center bg-transparent h-20 px-5 lg:mx-8 z-10">
+            <Image
+                src="/weiss.png"
+                alt="Metis Origins Logo"
+                width={130}
+                height={130}
+                className="object-contain"
+                priority
+            />
+
+             <div className="w-10 h-10 relative">
+            <AnimatePresence >
+                {open ? (
+                <motion.div
+                    key="menu"
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.8 }}
+                    transition={{ duration: 0.2 }}
+                    className="absolute inset-0 flex items-center justify-center"
+                    onClick={() => setOpen(false)}
+                >
+                    <RiCloseFill className="text-3xl" />
+                </motion.div>
+                ) : (
+                <motion.div
+                    key="close"
+                    initial={{ opacity: 0, scale: 0.8, rotate: -90 }}
+                    animate={{ opacity: 1, scale: 1, rotate: 0 }}
+                    exit={{ opacity: 0, scale: 0.8, rotate: 90 }}
+                    transition={{ duration: 0.2 }}
+                    onClick={() => setOpen(true)}
+                    className="absolute inset-0 flex items-center justify-center"
+                >
+                    <RiMenuFill className="text-3xl" />
+                </motion.div>
+                )}
+            </AnimatePresence>
+            </div>
+        </div>
+
+
+        {/* // Desktop navbar */}
+        <nav className="hidden relative lg:flex items-center justify-between px-8 py-6 mx-20 z-10">
+        {/* Logo */}
+        <Link href="/" className="flex items-center">
+            <Image
+            src="/weiss.png"
+            alt="Metis Origins Logo"
+            width={130}
+            height={130}
+            className="object-contain"
+            priority
+            />
+        </Link>
+
+        {/* Links */}
+        <div className="flex items-center gap-x-8 text-sm font-medium">
+            <Link href="/">Home</Link>
+            <Link href="/ueber-uns">About</Link>
+
+            {/* Botón "Kontakt" */}
+            <Link
+            href="/kontakt"
+            className="ml-2 inline-flex items-center justify-center rounded-full border border-white bg-white text-black px-5 py-2 font-semibold"
+            >
+            Kontakt
+            </Link>
+
+            {/* Iconos sociales */}
+            <div className="flex items-center gap-x-4 text-xl ml-2">
+            <Link
+                target="_blank"
+                href="https://www.instagram.com/metisorigins/"
+                aria-label="Instagram"
+                >
+                <RiInstagramFill />
+            </Link>
+            <Link
+                target="_blank"
+                href="https://www.linkedin.com/company/metisorigins/"
+                aria-label="LinkedIn"
+                >
+                <RiLinkedinFill />
+            </Link>
+            </div>
+        </div>
+        </nav>
+    </>
+  );
+};
---
+++ app/ui/components/home-components/HeroVideo.tsx
@@ -0,0 +1,50 @@
+"use client"
+import { useEffect, useState } from "react"
+
+interface Props {
+  src: string,
+  poster: string,
+  preloadBytes?: number
+  className?: string
+}
+
+export const HeroVideo = ({ src, poster, preloadBytes = 2000000, className = "absolute inset-0 w-full h-[80%] lg:h-full object-cover z-0 rounded-b-3xl" }: Props) => {
+  const [videoUrl, setVideoUrl] = useState<string>("");
+
+  useEffect(() => {
+    async function prefetchVideo() {
+      try {
+        const res = await fetch(src, {
+          headers: { Range: `bytes=0-${preloadBytes}` },
+        });
+        const blob = await res.blob();
+        const objectUrl = URL.createObjectURL(blob);
+        setVideoUrl(objectUrl);
+      } catch (error) {
+        console.error("Error prefetching video:", error);
+      }
+    }
+
+    prefetchVideo();
+
+    //clean memory on component unmount
+    return () => {
+      if (videoUrl) URL.revokeObjectURL(videoUrl);
+    };
+  }, [src, preloadBytes]);
+
+    return (
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        poster={poster}
+        className={className}
+      >
+        {videoUrl && <source src={videoUrl} type="video/mp4" />}
+        <source src="/metisoriginvideo.mp4" type="video/mp4"/>
+      </video>
+    );
+}
+
